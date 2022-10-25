e0a30376b1f2705d018112c51bb6bdc0d8db15b9
patrick okafor chibuike <patrickpeters911@gmail.com>
2022-10-25 01:55:33 +0100

    add tick and line through

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-var myList = document.getElementsByTagName('li');
+// Add an 'x' to each list elements
+var newList = document.getElementsByTagName('LI');
 
-for (let i = 0;  i < myList.length;  i++) {
+for (let i = 0; i < newList.length; i++) {
     var span = document.createElement('SPAN')
-    var txt = document.createTextNode('\u00D7')
-    span.className = 'cancel';
-    span.appendChild(txt);
+    var txt = document.createTextNode('\u00D7');
 
-    myList[i].appendChild(span);
-    
+    span.appendChild(txt)
+
+    span.className = 'cancel';
+    newList[i].appendChild(span)
+
 }
 
-var cancel = document.getElementsByClassName('cancel')
+// remove the toDo list by pressing the 'x' a.k.a cancel-class
 
-// for (let i = 0; i < cancel.length; i++) {
-//     cancel[i].onclick = () =>{
-//         var div = this.parentElement;
-//         div.style.display = 'none'
-//     };
-    
-// }
+var cancel = document.getElementsByClassName('cancel');
+
+for (let i = 0; i < cancel.length; i++) {
+    cancel[i].onclick = function () {
+        var closeList = this.parentElement
+        closeList.style.display = 'none'
+    };
+}
+
+var ulList = document.querySelector('ul');
+
+ulList.addEventListener('click', function(ev) {
+if (ev.target.tagName === 'LI') {
+ev.target.classList.toggle('checked');
+  }
+}, false);
 
 
-span.appendChild(cancel)
- 
-myList.appendChild(span)
-
+// function to create a new list by using the input value and appending to the ul
 
 function addTodo() {
     var list = document.createElement('li');
-    list.setAttribute('id', 'newList')
     var inputValue = document.getElementById('text').value;
 
     var textNode = document.createTextNode(inputValue);
@@ -37,12 +44,28 @@ function addTodo() {
 
     if (inputValue === '') {
         alert('you must todo something')
-        
-    }else{
-      
 
-    document.getElementById('ul').appendChild(list);
+    } else {
+        document.getElementById('ul').appendChild(list);
+    }
     document.getElementById('text').value = '';
+
+
+    // added the 'x' to the newly created list
+    var span = document.createElement('SPAN')
+    var txt = document.createTextNode('\u00D7');
+
+    span.className = 'cancel'
+    span.appendChild(txt)
+    list.appendChild(span)
+
+    // remove the new toDo list by pressing the 'x'
+
+    for (let i = 0; i < cancel.length; i++) {
+        cancel[i].onclick = function () {
+            var closeList = this.parentElement
+            closeList.style.display = 'none'
+        };
 
     }
 
@@ -50,4 +73,8 @@ function addTodo() {
 
 
 
- 
+
+
+
+
+
